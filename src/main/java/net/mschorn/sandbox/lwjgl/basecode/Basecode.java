b7648a0f000f57dea43283ee461e2e56f1ebce42
Michael Schorn <me@mschorn.net>
2012-06-02 16:23:22 +0200

    add basecode

---
+++ src/main/java/net/mschorn/sandbox/lwjgl/basecode/Basecode.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2012, Michael Schorn (me@mschorn.net). All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ * 
+ *   1. Redistributions of source code must retain the above copyright notice, this list of
+ *      conditions and the following disclaimer.
+ * 
+ *   2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ *      conditions and the following disclaimer in the documentation and/or other materials
+ *      provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
+package net.mschorn.sandbox.lwjgl.basecode;
+
+
+import javax.swing.JOptionPane;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextAttribs;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.PixelFormat;
+
+
+public final class Basecode {
+
+
+    private static final int WIDTH  = 800;
+    private static final int HEIGHT = 475;
+
+
+    private Basecode() {
+
+    }
+
+
+    private void glInit() {
+
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+
+    }
+
+
+    private void glDisplay() {
+
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+        GL11.glLoadIdentity();
+
+    }
+
+
+    private void glDispose() {
+
+    }
+
+
+    private void init() {
+
+        try {
+
+            Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+            Display.create(new PixelFormat(), new ContextAttribs(2, 1));
+            Display.setTitle("Basecode");
+
+        } catch (final LWJGLException e) {
+
+            JOptionPane.showMessageDialog(null, "Your Graphics Card does not support all necessary functions.", "OpenGL Context Creation", JOptionPane.ERROR_MESSAGE);
+
+            System.exit(1);
+
+        }
+
+
+    }
+
+
+    private void loop() {
+
+        glInit();
+
+        while (!Display.isCloseRequested()) {
+
+            glDisplay();
+
+            Display.update();
+        }
+
+        glDispose();
+
+        Display.destroy();
+
+    }
+
+
+    public static void main(final String[] args) {
+
+        final Basecode main = new Basecode();
+
+        main.init();
+        main.loop();
+
+    }
+
+}
