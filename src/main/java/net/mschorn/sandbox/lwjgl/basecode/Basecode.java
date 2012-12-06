f135b4ee7bea65b851a4dfc4780417c449648ce4
Michael Schorn <me@mschorn.net>
2012-12-06 01:24:18 +0100

    cleanup Basecode

--- src/main/java/net/mschorn/sandbox/lwjgl/basecode/Basecode.java
+++ src/main/java/net/mschorn/sandbox/lwjgl/basecode/Basecode.java
@@ -1,16 +1,16 @@
 /*
  * Copyright 2012, Michael Schorn (me@mschorn.net). All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification, are permitted
  * provided that the following conditions are met:
- * 
+ *
  *   1. Redistributions of source code must retain the above copyright notice, this list of
  *      conditions and the following disclaimer.
- * 
+ *
  *   2. Redistributions in binary form must reproduce the above copyright notice, this list of
  *      conditions and the following disclaimer in the documentation and/or other materials
  *      provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
  * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
@@ -19,7 +19,7 @@
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
  * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  */
 
 
@@ -28,39 +28,45 @@ package net.mschorn.sandbox.lwjgl.basecode;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
 import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glEnable;
 import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import net.mschorn.sandbox.lwjgl.tools.lifecycle.LWJGLLifecycle;
+import net.mschorn.sandbox.lwjgl.tools.window.LWJGLWindow;
 
-import javax.swing.JOptionPane;
-
-import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.ContextAttribs;
-import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.PixelFormat;
 
 
-public final class Basecode {
+public final class Basecode implements LWJGLLifecycle {
+
+    private static final int GL_VERSION_MAJOR = 1;
+    private static final int GL_VERSION_MINOR = 2;
+
+    private static final int WIDTH            = 800;
+    private static final int HEIGHT           = 475;
 
 
-    private static final int WIDTH  = 800;
-    private static final int HEIGHT = 475;
-
-
-    private Basecode() {
-
-    }
-
-
-    private void glInit() {
+    @Override
+    public void glInit(final int width, final int height) {
 
         glEnable(GL_DEPTH_TEST);
 
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
     }
 
 
-    private void glDisplay() {
+    @Override
+    public void glDisplay(final double timedelta) {
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glLoadIdentity();
@@ -68,57 +74,23 @@ public final class Basecode {
     }
 
 
-    private void glDispose() {
-
-    }
-
-
-    private void init() {
-
-        try {
-
-            Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
-            Display.create(new PixelFormat(), new ContextAttribs(1, 1));
-            Display.setTitle("Basecode");
-
-        } catch (final LWJGLException e) {
-
-            JOptionPane.showMessageDialog(null, "Your Graphics Card does not support all necessary functions.", "OpenGL Context Creation", JOptionPane.ERROR_MESSAGE);
-
-            System.exit(1);
-
-        }
-
-
-    }
-
-
-    private void loop() {
-
-        glInit();
-
-        while (!Display.isCloseRequested()) {
-
-            glDisplay();
-
-            Display.update();
-
-        }
-
-        glDispose();
-
-        Display.destroy();
+    @Override
+    public void glDispose() {
 
     }
 
 
     public static void main(final String[] args) {
 
-        final Basecode main = new Basecode();
+        final Basecode lifecycle = new Basecode();
+        final DisplayMode displayMode = new DisplayMode(WIDTH, HEIGHT);
+        final PixelFormat pixelFormat = new PixelFormat();
+        final ContextAttribs contextAttribs = new ContextAttribs(GL_VERSION_MAJOR, GL_VERSION_MINOR);
+        final String title = "Basecode";
 
-        main.init();
-        main.loop();
+        LWJGLWindow.display(lifecycle, displayMode, pixelFormat, contextAttribs, title);
 
     }
+
 
 }
